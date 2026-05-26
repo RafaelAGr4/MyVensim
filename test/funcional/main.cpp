d0c5eab3863bbbae6b2e4f3f30d59a8642eb386e
RafaelAGr4 <rafael.granato@aluno.ufop.edu.br>
2026-05-25 21:29:47 -0300

    Update main.cpp

--- test/funcional/main.cpp
+++ test/funcional/main.cpp
@@ -2,7 +2,11 @@
 #define MAIN_FUNCIONAL_TESTS
 
 #include "funcional_tests.h"
-#include "../../src/mySim.h" 
+#include "../../src/system.hpp"
+#include "../../src/flow.hpp"
+#include "../../src/model.hpp"
+#include "../../src/exponentialFlow.hpp"
+#include "../../src/logisticFlow.hpp"
 #include <iostream>
 
 int main() {
