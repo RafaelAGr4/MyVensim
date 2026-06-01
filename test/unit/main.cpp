dc6a42cf7ebad1f18c08904736aa0c46bd3bda31
Raagranatp <Raagranato@gmail.com>
2026-06-01 17:34:32 -0300

    Novos arquivos de teste unitario

---
+++ test/unit/main.cpp
@@ -0,0 +1,15 @@
+/// Arquivo main.cpp"
+#include "unit_tests.h"
+#include "unit_System.h"
+
+int main(){
+
+        run_unit_tests_globals();
+
+        run_unit_tests_System();
+
+        /* invoque outros metodos "void run_unit_tests_...(void)" aqui */
+
+        return true;
+
+}
---
+++ test/unit/unit_tests.cpp
@@ -0,0 +1,20 @@
+/// Arquivo unit_tests.cpp"
+#include <assert.h>
+#include "unit_test.h"
+#include "..\..\src\minhasFuncoes.h"
+
+  void unit_minhaFuncao1( void ){
+        /*insira o codigo de depuração, que invoca a função global ...minhaFuncao1(...) aqui"*/
+        assert(/*coloque a condição a ser verificada aqui*/);
+  }
+
+  void unit_minhaFuncao2( void ){
+        /*insira o codigo de depuração, que invoca a função global ...minhaFuncao2(...) aqui"*/
+        assert(/*coloque a condição a ser verificada aqui*/);
+  }
+
+  void run_unit_tests_globals( void ){
+        unit_minhaFuncao();
+        unit_minhaFuncao();
+  }
+
