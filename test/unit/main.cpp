b2bff780df622f7ab6682787ec5486bcd3fbf80a
Raagranatp <Raagranato@gmail.com>
2026-06-01 17:48:32 -0300

    Novo main de testes unitarios

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -1,15 +1,20 @@
 /// Arquivo main.cpp"
 #include "unit_tests.h"
 #include "unit_System.h"
+#include <iostream>
+using namespace std;
 
 int main(){
 
-        run_unit_tests_globals();
-
-        run_unit_tests_System();
-
-        /* invoque outros metodos "void run_unit_tests_...(void)" aqui */
-
-        return true;
+    run_unit_tests_System();
+    cout << "System [OK]" << endl;
+    run_unit_tests_Flow();
+    cout << "Flow [OK]" << endl;
+    run_unit_tests_FlowTypes();
+    cout << "FlowType [OK]" << endl;
+    run_unit_tests_Model();
+    cout << "Model [OK]" << endl;
+    cout << "All test [OK]!" << endl;
+    return 0;
 
 }
